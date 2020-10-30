780a46623aed86b5013a5e99c1f9397d7794931f
Jack Ellwood <jce22@duke.edu>
2020-10-30 14:52:18 -0400

    Added files to presentation

---
+++ doc/presentation1/BloonsApplication.java
@@ -0,0 +1,53 @@
+package ooga.visualization;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class BloonsApplication extends Application {
+
+  public static final double HEIGHT = 500;
+  public static final double WIDTH = 800;
+
+  Stage myStage;
+  Scene myScene;
+
+  @Override
+  public void start(Stage mainStage) {
+    myStage = mainStage;
+    BorderPane menuLayout = new BorderPane();
+    setupMenuLayout(menuLayout);
+    myScene = new Scene(menuLayout, WIDTH, HEIGHT);
+    myStage.setScene(myScene);
+    myStage.show();
+  }
+
+  private void setupMenuLayout(BorderPane menu) {
+    Text titleText = new Text("Bloons Tower Defense");
+    titleText.setScaleX(3);
+    titleText.setScaleY(3);
+    menu.setCenter(titleText);
+    Button startButton = new Button();
+    startButton.setOnAction(e -> loadLevel());
+    startButton.setText("Start");
+    BorderPane.setAlignment(startButton, Pos.CENTER);
+    menu.setBottom(startButton);
+  }
+
+  private void loadLevel() {
+    Group levelExample = new Group();
+    Rectangle gameSpace = new Rectangle(0, 0,3 * WIDTH / 4, 7 * HEIGHT / 8);
+    gameSpace.setFill(Color.GREENYELLOW);
+    levelExample.getChildren().add(gameSpace);
+    myScene = new Scene(levelExample, WIDTH, HEIGHT);
+    myScene.setFill(Color.LIGHTGRAY);
+    myStage.setScene(myScene);
+  }
+}
---
+++ doc/presentation1/EXAMPLE_USE_CASES.md
@@ -0,0 +1,11 @@
+### Popping a higher level balloon releases lower level balloons
+* When a higher level balloon is popped, it can either downgrade into a lower level balloon (i.e blue → red) or it can release a lot of lower-level balloons
+* Precondition: The game is running
+* Primary actor: Bloon object/game
+* Trigger: A balloon is popped
+
+### AOE upgrade
+* A player clicks on the monkey’s upgrade menu and clicks on AOE upgrade which lets the monkey hit an AOE area from the first balloon it aims at
+* Precondition: Monkey is in play and the menu is opened
+* Primary actor: Player and Monkey
+* Trigger: The player clicks the upgrade button
