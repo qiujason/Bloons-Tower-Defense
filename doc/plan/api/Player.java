a4cfe0d7a6c9348c6b45aa03dd09e0e02f4129d2
frwu93 <franklinw888@gmail.com>
2020-10-29 20:43:10 -0400

    added player.java api in plan

---
+++ doc/plan/api/Player.java
@@ -0,0 +1,18 @@
+/**
+ * This interface acts as the API for a player's functionality.
+ */
+public interface Player{
+
+  public void clickButton();
+
+  public void placeTower();
+
+  public void selectLanguage();
+
+  public void startGame();
+
+  public void switchGame();
+
+  public void exitToMenu();
+
+}
