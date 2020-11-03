17f0eda7b51c37ed28de88208595dda21cc12e65
frwu93 <franklinw888@gmail.com>
2020-11-03 16:46:39 -0500

    created layout

--- src/ooga/backend/layout/Layout.java
+++ src/ooga/backend/layout/Layout.java
@@ -1,13 +1,30 @@
 package ooga.backend.layout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Layout {
 
   int width;
   int height;
+  List<List<LayoutBlock>> layoutConfig;
 
-  public Layout(String[][] layoutConfig){
-    this.width = layoutConfig[0].length;
-    this.height = layoutConfig.length;
+  public Layout(List<List<String>> layoutConfig){
+    this.width = layoutConfig.get(0).size();
+    this.height = layoutConfig.size();
+    this.layoutConfig = createLayoutConfig(layoutConfig);
+  }
+
+  private List<List<LayoutBlock>> createLayoutConfig(List<List<String>> layoutConfig) {
+    List<List<LayoutBlock>> layoutBlocks = new ArrayList<>();
+    for (List<String> row : layoutConfig) {
+      List<LayoutBlock> blockRow = new ArrayList<>();
+      for (String blockType : row) {
+        blockRow.add(new LayoutBlock(blockType));
+      }
+      layoutBlocks.add(blockRow);
+    }
+    return layoutBlocks;
   }
 
 
