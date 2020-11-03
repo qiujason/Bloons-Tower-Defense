3416ae1a070165e55cc316f3e4f6b06e6bbd4db1
Eddie Kong <dk255@duke.edu>
2020-11-03 15:31:16 -0500

    merged master into branch and created properties file for menu buttons

--- src/ooga/visualization/GameMenu.java
+++ src/ooga/visualization/GameMenu.java
@@ -1,34 +1,35 @@
 package ooga.visualization;
 
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.layout.BorderPane;
 import ooga.controller.MenuInterface;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-
 
 public class GameMenu {
 
-  private VBox menuPane;
-  private MenuInterface menuController;
+  private static final String LANGUAGE = "English";
+  private final ResourceBundle menuProperties = ResourceBundle
+      .getBundle(getClass().getPackageName() + ".resources.menu" + LANGUAGE);
+
+  private BorderPane myMenuPane;
 
   private Button playButton;
   private Button pauseButton;
   private Button speedButton;
 
-  private static final String PLAY_TEXT = "Play";
-  private static final String PAUSE_TEXT = "Pause";
-  private static final String SPEEDUP_TEXT = "SpeedUp";
-  private static final String SLOWDOWN_TEXT = "SlowDown";
+  private static final String PLAY_TEXT = "PlayButton";
+  private static final String PAUSE_TEXT = "PauseButton";
+  private static final String SPEEDUP_TEXT = "SpeedUpButton";
+  private static final String SLOWDOWN_TEXT = "SlowDownButton";
 
-  public GameMenu(VBox myMenuPane, MenuInterface myController) {
-    menuPane = myMenuPane;
-    menuController = myController;
+  public GameMenu(BorderPane MenuPane, MenuInterface controller) {
+    myMenuPane = MenuPane;
 
-    playButton = makeButton(PLAY_TEXT, event -> menuController.play());
-    pauseButton = makeButton(PAUSE_TEXT, event -> menuController.pause());
-    speedButton = makeButton(SPEEDUP_TEXT, event -> menuController.speedUp());
-
+    playButton = makeButton(menuProperties.getString(PLAY_TEXT), event -> controller.play());
+    pauseButton = makeButton(menuProperties.getString(PAUSE_TEXT), event -> controller.pause());
+    speedButton = makeButton(menuProperties.getString(SPEEDUP_TEXT), event -> controller.speedUp());
   }
 
   /**
@@ -40,7 +41,7 @@ public class GameMenu {
     button.setText(name);
     button.setOnAction(handler);
     button.setId(name);
-    menuPane.getChildren().add(button);
+    myMenuPane.getChildren().add(button);
     return button;
   }
 }
