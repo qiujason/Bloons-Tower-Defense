6191d066d192c6c126cf1480840735be3f461ae4
Eddie Kong <dk255@duke.edu>
2020-11-03 16:34:38 -0500

    created tests for if the buttons were added to the stage

--- test/visualization/BloonsApplicationTest.java
+++ test/visualization/BloonsApplicationTest.java
@@ -11,12 +11,18 @@ import util.DukeApplicationTest;
 public class BloonsApplicationTest extends DukeApplicationTest {
 
   Button myStartButton;
+  Button myPlayButton;
+  Button myPauseButton;
+  Button mySpeedUpButton;
 
   @Override
   public void start(Stage testStage){
     BloonsApplication myBloonsApplication = new BloonsApplication();
     myBloonsApplication.start(testStage);
     myStartButton = lookup("#Start").query();
+    myPlayButton = lookup("#Play").query();
+    myPauseButton = lookup("#Pause").query();
+    mySpeedUpButton = lookup("#SpeedUp").query();
   }
 
   @Test
@@ -28,6 +34,9 @@ public class BloonsApplicationTest extends DukeApplicationTest {
   public void testStartButton(){
     clickOn(myStartButton);
     // TODO: Add assertion
+    assertEquals("Play", myPlayButton.getText());
+    assertEquals("Pause", myPauseButton.getText());
+    assertEquals("SpeedUp", mySpeedUpButton.getText());
   }
 
 
