4b65751337431f01df1a5ab6f86d56df90c5106c
Ann-Shine Wu <annshine.wu@duke.edu>
2020-11-06 21:51:03 -0500

    implemented TowersCollection and TowerIterator

--- src/ooga/backend/towers/TowersCollection.java
+++ src/ooga/backend/towers/TowersCollection.java
@@ -1,33 +1,50 @@
 package ooga.backend.towers;
 
+import java.util.ArrayList;
+import java.util.List;
 import ooga.backend.API.GamePiece;
 import ooga.backend.collections.GamePieceCollection;
 import ooga.backend.collections.Iterator;
 
 public class TowersCollection implements GamePieceCollection {
 
+  List<Tower> towers;
+
+  public TowersCollection(){
+    towers = new ArrayList<>();
+  }
+
   @Override
   public boolean add(GamePiece gamePiece) {
+    if (gamePiece instanceof Tower) {
+      towers.add((Tower)gamePiece);
+      return true;
+    }
     return false;
   }
 
   @Override
   public boolean remove(GamePiece gamePiece) {
+    if (gamePiece instanceof Tower) {
+      return towers.remove(gamePiece);
+    }
     return false;
   }
 
   @Override
   public void updateAll() {
-
+    for (Tower tower : towers) {
+      tower.update();
+    }
   }
 
   @Override
   public void clear() {
-
+    towers = new ArrayList<>();
   }
 
   @Override
   public Iterator createIterator() {
-    return null;
+    return new TowersIterator(towers);
   }
 }
