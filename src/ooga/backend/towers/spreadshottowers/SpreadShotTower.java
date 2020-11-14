7fdbfafc512504847a4152e218f0a9098536d44e
Ann-Shine Wu <annshine.wu@duke.edu>
2020-11-14 18:03:34 -0500

    added property files for towers to be customizable and added upgrades

--- src/ooga/backend/towers/spreadshottowers/SpreadShotTower.java
+++ src/ooga/backend/towers/spreadshottowers/SpreadShotTower.java
@@ -1,11 +1,20 @@
 package ooga.backend.towers.spreadshottowers;
 
+import java.util.ArrayList;
+import java.util.List;
+import ooga.backend.bloons.BloonsCollection;
+import ooga.backend.projectile.Projectile;
+import ooga.backend.projectile.ProjectileType;
+import ooga.backend.projectile.factory.ProjectileFactory;
+import ooga.backend.projectile.factory.SingleProjectileFactory;
 import ooga.backend.towers.Tower;
 
 public abstract class SpreadShotTower extends Tower {
 
   private static final int numberOfShots = 8;
   private static final int degreeIncrementPerShot = 45;
+
+  private ProjectileType projectileType;
 
   public SpreadShotTower(double myXPosition, double myYPosition, double myRadius,
       double myShootingSpeed, double myShootingRestRate) {
@@ -18,5 +27,27 @@ public abstract class SpreadShotTower extends Tower {
 
   public int getDegreeIncrementPerShot(){
     return degreeIncrementPerShot;
+  }
+
+  public void setProjectileType(ProjectileType update){
+    projectileType = update;
+  }
+
+  public abstract ProjectileType getProjectileType();
+
+  @Override
+  public List<Projectile> shoot(BloonsCollection bloonsCollection) {
+    updateCanShoot(false);
+    List<Projectile> shot = new ArrayList<>();
+    if(checkBalloonInRange(bloonsCollection)){
+      for(int i = 0; i < getNumberOfShots(); i++){
+        ProjectileFactory projectileFactory = new SingleProjectileFactory();
+        double projectileXVelocity = Math.cos(i*getDegreeIncrementPerShot());
+        double projectileYVelocity = Math.sin(i*getDegreeIncrementPerShot());
+        shot.add(projectileFactory
+            .createDart(projectileType, getXPosition(), getYPosition(), projectileXVelocity, projectileYVelocity));
+      }
+    }
+    return shot;
   }
 }
