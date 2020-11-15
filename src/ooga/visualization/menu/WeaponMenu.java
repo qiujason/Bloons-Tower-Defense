5b1577a198497c2faa7634441d34e34ff2d20939
Eddie Kong <dk255@duke.edu>
2020-11-15 03:16:12 -0500

    working on closing the tower menu

--- src/ooga/visualization/menu/WeaponMenu.java
+++ src/ooga/visualization/menu/WeaponMenu.java
@@ -17,16 +17,19 @@ public class WeaponMenu extends FlowPane {
   private static final String UPGRADE_RANGE_TEXT = "RangeUpgradeButton";
   private static final String UPGRADE_RATE_TEXT = "RateUpgradeButton";
   private static final String SELL_TOWER_TEXT = "SellTowerButton";
+  private static final String CLOSE_MENU_TEXT = "CloseMenuButton";
   private static final Double BUTTON_WIDTH = 100.0;
 
   private Button upgradeRangeButton;
   private Button upgradeRateButton;
   private Button sellTowerButton;
+  private Button closeMenuButton;
 
   public WeaponMenu(TowerNode tower, TowerMenuInterface controller){
     upgradeRangeButton = makeButton(menuProperties.getString(UPGRADE_RANGE_TEXT), event -> controller.upgradeTower(tower));
     upgradeRateButton = makeButton(menuProperties.getString(UPGRADE_RATE_TEXT), event -> controller.upgradeTower(tower));
     sellTowerButton = makeButton(menuProperties.getString(SELL_TOWER_TEXT), event -> controller.sellTower(tower));
+    closeMenuButton = makeButton(menuProperties.getString(CLOSE_MENU_TEXT), event -> controller.closeMenu());
   }
 
   private Button makeButton(String name, EventHandler<ActionEvent> handler) {
