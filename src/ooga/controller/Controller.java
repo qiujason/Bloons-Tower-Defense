6a17c33d02f5e5f4abc22e7fcc41c589b6f29eb7
Ann-Shine Wu <annshine.wu@duke.edu>
2020-11-19 01:48:53 -0500

    changed round bonus file

--- src/ooga/controller/Controller.java
+++ src/ooga/controller/Controller.java
@@ -44,7 +44,7 @@ public class Controller extends Application {
   public static final String TOWER_BUY_VALUES_PATH = "towervalues/TowerBuyValues.properties";
   public static final String TOWER_SELL_VALUES_PATH = "towervalues/TowerSellValues.properties";
   public static final String ROAD_ITEM_VALUES_PATH = "towervalues/roadItemBuyValues.properties";
-  public static final String ROUND_BONUSES_PATH = "roundBonuses/BTD5_default_level1_to_10.csv";
+  public static final String ROUND_BONUSES_PATH = "roundBonuses/1000Rounds_defaultStartingBonus.csv";
 
   private ResourceBundle errorResource;
   private Timeline myAnimation;
