c4e5c99cf85e6ea34b5632b1c3d0616c87eb2083
Jack Ellwood <jce22@duke.edu>
2020-11-23 01:06:31 -0500

    Changes to stylesheets

--- src/ooga/visualization/BloonsApplication.java
+++ src/ooga/visualization/BloonsApplication.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
