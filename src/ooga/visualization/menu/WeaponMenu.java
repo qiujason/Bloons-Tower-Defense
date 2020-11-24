79c8dc3c4ef03bf6a9af5ad1493e8b555edc5bce
Eddie Kong <donggyun.kong@duke.edu>
2020-11-24 17:23:48 -0500

    Revert "Merge branch 'revert-d6eaf219' into 'master'"
    This reverts merge request !101

--- src/ooga/visualization/menu/WeaponMenu.java
+++ src/ooga/visualization/menu/WeaponMenu.java
@@ -6,9 +6,12 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 import ooga.backend.towers.ShootingChoice;
-import ooga.controller.WeaponNodeHandler;
+import ooga.controller.WeaponNodeInterface;
 import ooga.visualization.nodes.TowerNode;
 
+/**
+ * This class holds the menu for individual towers for upgrades, shooting choices, and selling
+ */
 public class WeaponMenu extends FlowPane {
 
   private static final String RESOURCE_DIRECTORY = "ooga.visualization.resources.languages.";
@@ -23,7 +26,16 @@ public class WeaponMenu extends FlowPane {
   private ResourceBundle menuProperties;
   private ShootingChoiceBox shootingChoiceBox = null;
 
-  public WeaponMenu(TowerNode tower, WeaponNodeHandler weaponNodeHandler, String language) {
+  /**
+   * Constructor that creates the WeaponMenu for the TowerNode it should be connected to. Makes
+   * all of the buttons and connects the functionality from the WeaponNodeInterface.
+   *
+   * @param tower the selected TowerNode that the WeaponMenu should be connected to
+   * @param weaponNodeHandler the WeaponNodeInterface holding the functionality the WeaponMenu buttons
+   *                          should perform
+   * @param language the language that the game is set in
+   */
+  public WeaponMenu(TowerNode tower, WeaponNodeInterface weaponNodeHandler, String language) {
     initializeResourceBundle(language);
     this.setPrefWrapLength(200);
     makeButton(menuProperties.getString(UPGRADE_RANGE), event -> weaponNodeHandler
--- src/ooga/visualization/nodes/ItemNodeFactory.java
+++ src/ooga/visualization/nodes/ItemNodeFactory.java
@@ -2,8 +2,19 @@ package ooga.visualization.nodes;
 
 import ooga.backend.roaditems.RoadItemType;
 
+/**
+ * This interface is used to create a front-end road item node.
+ */
 public interface ItemNodeFactory {
 
+  /**
+   * Creates a RoadItemNode based on its type, the given coordinates, and the radius.
+   * @param roadItemType the type of RoadItem that is being made
+   * @param xPosition the x position of the RoadItemNode
+   * @param yPosition the y position of the RoadItemNode
+   * @param radius the radius of the RoadItemNode
+   * @return the created RoadItemNode
+   */
   RoadItemNode createItemNode(RoadItemType roadItemType, double xPosition, double yPosition,
       double radius);
 
